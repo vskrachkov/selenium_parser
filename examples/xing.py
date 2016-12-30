411db54d059c224d5e052a885bea6e7d5bee71f5
Vyacheslav Krachkov <vskrachkov@gmail.com>
2016-12-30 22:55:48 +0200

    fixed problem with companies list in examples

--- examples/xing.py
+++ examples/xing.py
@@ -1,14 +1,15 @@
 import csv
-import time
-import string
 import logging
+import string
+import sys
+import time
 
 from selenium import webdriver
-# from pyvirtualdisplay import Display
+from pyvirtualdisplay import Display
 
 
 # setup logging
-logging.basicConfig(filename='xing.log', level=logging.DEBUG,
+logging.basicConfig(filename='xing.log', level=logging.INFO,
                     format='%(asctime)s %(message)s')
 
 logging.info('Creating result file.')
@@ -16,36 +17,46 @@ result_file = open('result.csv', 'w')
 csw_writer = csv.writer(result_file)
 
 logging.info('Starting virtual display.')
-# display = Display(visible=0, size=(800, 600))
-# display.start()
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+
+class PlatformException(Exception):
+    """Platform exception."""
+
 
 logging.info('Starting chrome_driver.')
-driver = webdriver.Chrome('../drivers/chromedriver')
+if sys.platform == 'linux2':
+    driver = webdriver.Chrome('../drivers/chromedriver_linux')
+elif sys.platform == 'darwin':
+    driver = webdriver.Chrome('../drivers/chromedriver_mac')
+else:
+    raise PlatformException
+
 
 logging.info('Getting initial url for searching companies.')
-companies = ('https://www.xing.com/companies/industries' \
-             '/170000-marketing-pr-and-design?page={}'.format(p)
-             for p in range(1, 280 + 1))
-
+# companies = ('https://www.xing.com/companies/industries'
+#              '/170000-marketing-pr-and-design?page={}'.format(p)
+#              for p in range(1, 280 + 1))
+companies = ('https://www.xing.com/companies/industries'
+             '/170000-marketing-pr-and-design', )
 for company in companies:
-    logging.info(company)
+    logging.info('Company: {}'.format(company))
     driver.get(company)
     logging.info('Getting link to company page and go by this link.')
-    elements = (elem.get_property('href') for elem
-                in driver.find_elements_by_xpath('//a[@class="company-link"]'))
+    elements = [elem.get_property('href') for elem
+                in driver.find_elements_by_xpath('//a[@class="company-link"]')]
+    elements = list(elements)
     for href in elements:
         print(href)
-        print( '=' * 20)
-        try:
-            driver.get(href)
-            logging.info('Go to employees tab.')
-        except:
-            print( 'except here !!!')
+        print('=' * 20)
+        driver.get(href)
+        logging.info('Go to employees tab.')
         try:
             a = driver.find_element_by_xpath('//li[@id="employees-tab"]/a')
             a.click()
         except Exception as err:
-            logging.warning(
+            logging.info(
                 '{} error when trying to get employees tab.'.format(err))
         logging.info('Getting all employees of company')
         time.sleep(.5)
@@ -63,12 +74,13 @@ for company in companies:
 
                         csw_writer.writerow(cols)
             except Exception as err:
-                logging.warning(
-                    '{} error when trying to get employee profile info'.format(err))
+                logging.info('{} error when trying to get '
+                             'employee profile info'.format(err))
                 continue
+        logging.info('Go to 2 pages back')
         driver.execute_script("window.history.go(-2)")
 
 logging.info('Closing browser ...')
 driver.close()
 logging.info('Stopping display ...')
-# display.stop()
+display.stop()
